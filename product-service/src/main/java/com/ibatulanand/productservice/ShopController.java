b6da48be11baf0932181b2d56c81ca7195505495
Angie <angie.saudade@gmail.com>
2025-05-08 22:55:06 -0400

    Added a new controller

---
+++ product-service/src/main/java/com/ibatulanand/productservice/ShopController.java
@@ -0,0 +1,23 @@
+package com.ibatulanand.productservice;
+
+import com.ibatulanand.productservice.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+
+    private final ProductService productService;
+
+    public ShopController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/shop")
+    public String showShop(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "products";          // looks for templates/products.html
+    }
+}
+
